20f4cf0f4505bd017e632c5e92d6a3af8e246e9c
Fathi Ayari <fathi3ayari333@gmail.com>
2021-11-02 10:50:33 +0100

    first commit

--- app/src/main/java/com/fathi_ayari/affirmations/MainActivity.kt
+++ app/src/main/java/com/fathi_ayari/affirmations/MainActivity.kt
@@ -2,10 +2,17 @@ package com.fathi_ayari.affirmations
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.fathi_ayari.affirmations.adapter.ItemAdapter
+import com.fathi_ayari.affirmations.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val myDataset = Datasource().loadAffirmations()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+        recyclerView.setHasFixedSize(true)
     }
 }
